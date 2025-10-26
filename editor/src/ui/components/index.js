2a3c557979de6e41e91f580e8909557328620574
Tenderworld <32334106+Amirashkan@users.noreply.github.com>
2025-10-26 16:26:56 +0330

    chore: Remove editor/src from tracking (local Windows workaround)

--- editor/src/ui/components/index.js
+++
@@ -1,5 +0,0 @@
-// src/ui/components/index.js
-export { FileInputHandler } from './FileInputHandler.js';
-export { SelectInputHandler } from './SelectInputHandler.js';
-export { TextInputHandler } from './TextInputHandler.js';
-export { ParameterValueManager } from './ParameterValueManager.js';
